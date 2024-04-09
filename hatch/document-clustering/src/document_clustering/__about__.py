9f9506090a7ea5fe37e07f8c574c4d75e523f292
Leona Gottfried <61663141+leogott@users.noreply.github.com>
2024-04-09 13:31:12 +0200

    hatch new files

---
+++ hatch/document-clustering/src/document_clustering/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Leona Gottfried <61663141+leogott@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ hatch/document-clustering/src/document_clustering/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Leona Gottfried <61663141+leogott@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
