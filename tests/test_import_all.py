7eb985e7f096e20424458a4dd0d14964068be2c0
Leona Gottfried <61663141+leogott@users.noreply.github.com>
2024-05-31 16:14:53 +0200

    Polish

--- tests/test_import_all.py
+++ tests/test_import_all.py
@@ -5,6 +5,6 @@ def test_import_all():
     from document_clustering import arxiv_dataset
     from document_clustering import clustering
     from document_clustering import generate_sample
-    from document_clustering import main
+    from document_clustering import preprocessing
     from document_clustering import pdf_extract
     from document_clustering import utils
